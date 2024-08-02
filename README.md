9d5ab2d6e9c453b8bde34770705618cfbb5c4335
Clink_Y99 <clinekieran@gmail.com>
2024-08-02 12:10:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TwitchGameJame2024
+
+Developed with Unreal Engine 5
---
+++ Source/TwitchGameJame2024/TwitchGameJame2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TwitchGameJame2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwitchGameJame2024, "TwitchGameJame2024" );
